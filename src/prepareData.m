ca9db7c46a1b64305fee26a498046fbca0ff24ea
Milind Ganjoo <mganjoo@stanford.edu>
2012-11-17 13:46:58 -0800

    Move generation of p matrix for backpropagation outer product inside category loop; saves more memory that way

--- src/prepareData.m
+++ src/prepareData.m
@@ -8,10 +8,7 @@ data.categories = categories;
 data.wordTable = wordTable;
 
 % [ [ w_i repeated m times for each image ] for all k categories ]
-p1 = wordTable(:, reshape(repmat(1:numCategories, numImages, 1), 1, []));
-% [ im_1 .. im_m (all images) ] repeated k times for each category
-p2 = repmat(imgs, 1, numCategories);
-data.p = [ p1; p2 ];
+data.p1 = wordTable(:, reshape(repmat(1:numCategories, numImages, 1), 1, []));
 
 % prepare list of 'good' indices
 data.goodIndices = (categories-1) * numImages + (1:numImages);
