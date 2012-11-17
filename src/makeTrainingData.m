ca32764fb1dcb58ca146953d99964a129a368ffb
Milind Ganjoo <mganjoo@stanford.edu>
2012-11-17 11:12:22 -0800

    Add convenience script to generate training data

---
+++ src/makeTrainingData.m
@@ -0,0 +1,12 @@
+% Prepare the training data
+
+X = load('../image_data/trainX.mat');
+Y = load('../image_data/trainY.mat');
+
+disp('Creating training set');
+makeFeatureBatches(X.trainXCs, Y.trainY, 5, { 'cat', 'truck' });
+
+disp('Creating mini-training set');
+makeFeatureBatches(X.trainXCs, Y.trainY, 5, { 'cat', 'truck' }, 4, 'mini-batch');
+
+clear X Y;
