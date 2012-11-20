9b79db39016c3db31a4bf70d1cd7885427508d6f
Milind Ganjoo <mganjoo@stanford.edu>
2012-11-19 17:50:14 -0800

    Change the way the confusion marix is displayed (change tabs to spaces)

--- src/displayConfusionMatrix.m
+++ src/displayConfusionMatrix.m
@@ -2,13 +2,13 @@ function [] = displayConfusionMatrix( confusion, categoryLabels )
 
 numCategories = length(categoryLabels);
 disp('Confusion matrix:');
-str = sprintf('...\t');
+str = sprintf('%-7s', '...');
 for i = 1:numCategories
     categ = char(categoryLabels(i));
     if length(categ) > 4
         categ = categ(1:4);
     end
-    str = sprintf('%s%s\t', str, categ);
+    str = sprintf('%s%-7s', str, categ);
 end
 str = sprintf('%s\n', str);
 disp(str);
@@ -17,9 +17,9 @@ for i = 1:numCategories
     if length(categ) > 4
         categ = categ(1:4);
     end
-    str = sprintf('%s\t', categ);
+    str = sprintf('%-7s', categ);
     for j = 1:numCategories
-        str = sprintf('%s%d\t', str, confusion(i, j));
+        str = sprintf('%s%-7d', str, confusion(i, j));
     end
     str = sprintf('%s\n', str);
     disp(str);
