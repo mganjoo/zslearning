463834d763bdc2e3b8cb452a101c1fdce9d147ef
Milind Ganjoo <mganjoo@stanford.edu>
2012-11-21 10:33:29 -0800

    Fix bug with test data generation

---
+++ makeTestBatch.m
@@ -0,0 +1,57 @@
+function [] = makeTestBatch(trainX, trainY, excludedCategories, prefix, numPerCategory)
+
+% Load categories
+b = load('image_data/cifar-10-batches-mat/batches.meta.mat');
+categoryNames = b.label_names;
+
+% CIFAR constants (do not modify)
+TOTAL_IMAGES = 10000;
+totalCategories = length(categoryNames);
+MAX_NUM_PER_CATEGORY = TOTAL_IMAGES / totalCategories;
+
+if nargin < 5
+    numPerCategory = MAX_NUM_PER_CATEGORY;
+    if nargin < 4
+        prefix = 'default_test_batch';
+        if nargin < 3 
+            excludedCategories = {};
+        else
+            disp('Excluded categories:');
+            for i = 1:length(excludedCategories)
+                disp(excludedCategories{i});
+            end
+        end
+    end
+end
+
+assert(numPerCategory <= MAX_NUM_PER_CATEGORY);
+fprintf('Putting %d samples per category in each file\n', numPerCategory);
+
+numCategories = totalCategories - length(excludedCategories);
+batch = zeros(1, numPerCategory * numCategories);
+k = 1;
+categorySet = zeros(1, numCategories);
+for i = 1:totalCategories
+    if ismember(categoryNames(i), excludedCategories)
+        continue;
+    end
+    categorySet(k) = i;
+    temp = find(trainY == i);
+    batch((k-1)*numPerCategory+1:k*numPerCategory) = temp(1:numPerCategory);
+    k = k + 1;
+end
+
+% Create directory if it doesn't exist
+if not(exist('image_data/cifar-10-features', 'dir'))
+    mkdir('image_data/cifar-10-features');
+end
+
+batch = batch(randperm(length(batch)));
+disp('Output test batch');
+t = matfile(sprintf('image_data/cifar-10-features/%s.mat', prefix));
+t.trainX = trainX(:, batch);
+trainYc = arrayfun(@(x) find(categorySet == x), trainY(:, batch));
+t.trainY = trainYc;
+t.names = categoryNames(categorySet);
+
+end
--- makeTrainingData.m
+++ makeTrainingData.m
@@ -1,4 +1,4 @@
-% Prepare the training data
+% Prepare the data
 
 train = load('image_data/train.mat');
 test = load('image_data/test.mat');
@@ -10,6 +10,6 @@ disp('Creating mini-training set');
 makeFeatureBatches(train.trainX, train.trainY, 5, { 'cat', 'truck' }, 'mini_batch', 4);
 
 disp('Creating zero-shot test set');
-makeFeatureBatches(test.testX, test.testY, 1, { 'airplane', 'automobile', 'bird', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck' }, 'zeroshot_test_batch');
+makeTestBatch(test.testX, test.testY, { 'airplane', 'automobile', 'bird', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck' }, 'zeroshot_test_batch');
 
 clear train test;
