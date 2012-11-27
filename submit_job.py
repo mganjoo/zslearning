c9a8be148e751e64a839b753667c35d5681f2508
Milind Ganjoo <mganjoo@stanford.edu>
2012-11-26 22:44:09 -0800

    Revamp submit script for NLP cluster

--- submit_job.py
+++ submit_job.py
@@ -1,42 +1,84 @@
 #!/usr/bin/python
 
 import os
+import sys
+import argparse
+import datetime
 
-PROJECT_PATH = '/afs/ir/users/m/g/mganjoo/projects/vsm-learning/'
+# The project path is always the directory of the script
+PROJECT_PATH = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 matlabCommand = """
-module load MATLAB-R2012b
-matlab -nodesktop <<EOD
+matlab -nodisplay -nodesktop <<EOD
 cd {projectPath:s};
 trainParams.wordDataset     = {wordDataset!r};
 trainParams.batchFilePrefix = {batchFilePrefix!r};
 trainParams.maxPass         = {maxPass:d};
 trainParams.maxIter         = {maxIter:d};
-trainParams.cReg            = {cReg:.3E};
+trainParams.wReg            = {wReg:.3E};
+trainParams.iReg            = {iReg:.3E};
 trainParams.outputPath      = {outputPath!r};
 train;
 EOD
 """
 
-jobNum = 1
-for iter in [ 3, 4, 6, 8 ]:
-    jobName = "vsm{0:03d}".format(jobNum)
-    outputPath = PROJECT_PATH + "savedParams-%s/" % jobName
+qsubCommand = 'cat <<EOF | nlpsub -x "-V" -M bea -n {name} -d {outputPath} --clobber -p high {optionalArgs} {command}EOF'
 
+parser = argparse.ArgumentParser(description="Submit training job to NLP cluster")
+parser.add_argument('--wordset', help='the word vector dataset to use')
+parser.add_argument('--trainset', help='the prefix of the image batches to use')
+parser.add_argument('--maxPass', help='the number of passes through the training data', type=int)
+parser.add_argument('--maxIter', help='the number of iterations over a training batch', type=int)
+parser.add_argument('--wordReg', help='the regularization param for words', type=float)
+parser.add_argument('--imageReg', help='the regularization param for images', type=float)
+parser.add_argument('-m', '--machine', nargs='*', help='machines to use')
+parser.add_argument('--mem', help='memory to allocate')
+parser.add_argument('-i', '--jobId', help='the name of the job, also used as output folder')
+parser.add_argument('-v', '--verbose', help='show command that will be executed', action="store_true")
+args = parser.parse_args()
+
+jobId = args.jobId or ('zsl-' + datetime.datetime.today().strftime("%Y-%m-%d_%H%M%S_%f"))
+outputPath = os.path.join(PROJECT_PATH, jobId)
+
+arguments = {
+    'wordDataset':     args.wordset or 'icml',
+    'batchFilePrefix': args.trainset or 'mini_batch',
+    'maxPass':         args.maxPass or 3,
+    'maxIter':         args.maxIter or 2,
+    'wReg':            args.wordReg or 1E-3,
+    'iReg':            args.imageReg or 1E-6,
+    'projectPath':     PROJECT_PATH,
+    'outputPath':      outputPath,
+}
+
+optionalArgs = ''
+if args.machine:
+    optionalArgs += ' -H {0} '.format(' '.join(args.machine))
+if args.mem:
+    optionalArgs += ' -m {0} '.format(args.mem)
+
+qsubArguments = {
+    'name':         jobId,
+    'outputPath':   outputPath,
+    'optionalArgs': optionalArgs.strip(),
+    'command':      matlabCommand.format(**arguments),
+}
+
+finalCommand = qsubCommand.format(**qsubArguments)
+if args.verbose:
+    print '$ ' + finalCommand
+
+# if none of the arguments exist, don't submit job
+atLeastOne = False
+argDict = vars(args)
+for argKey in argDict:
+    if argDict[argKey] and argKey != 'verbose':
+        atLeastOne = True
+        break
+
+if atLeastOne:
     if not os.path.exists(outputPath):
         os.makedirs(outputPath)
-
-    arguments = {
-        'wordDataset':     'icml',
-        'batchFilePrefix': 'default_batch',
-        'maxPass':         70,
-        'maxIter':         iter,
-        'cReg':            1E-3,
-        'projectPath':     PROJECT_PATH,
-        'outputPath':      outputPath
-    }
-
-    qsubCommand = "cat <<EOF | qsub -V -m be -M mganjoo@stanford.edu -N {0} -o {1} -e {2}{3}EOF"
-
-    os.system(qsubCommand.format(jobName, outputPath + "out", outputPath + "err", matlabCommand.format(**arguments)))
-    jobNum = jobNum + 1
+    os.system(finalCommand)
+else:
+    print '>>>> No argument was given. Not submitting job. You can see the way the argument would be constructed.'
