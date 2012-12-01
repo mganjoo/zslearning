55d68449eb9248898e0bd7f4a04d75bcd7613da6
Milind Ganjoo <mganjoo@stanford.edu>
2012-11-30 19:15:02 -0800

    Change training cost function to linear

--- mapTrainingCost.m
+++ mapTrainingCost.m
@@ -7,17 +7,16 @@ numCategories = size(data.wordTable, 2);
 numImages     = size(data.imgs, 2);
 
 % Feedforward
-z2 = bsxfun(@plus, W * data.imgs, b);
-h = params.f(z2);
+h = bsxfun(@plus, W * data.imgs, b);
 
 [ t1,t2 ] = meshgrid(1:numCategories, 1:numImages);
 diff = h(:,t2(:)) - data.wordTable(:,t1(:));
-cost = 0.5 / numImages / 1000 * sum(sum((diff) .^ 2));
+cost = 0.5 / numImages * sum(sum((diff) .^ 2));
 
 sumdiff = reshape(sum(reshape(diff, params.outputSize * numImages, []), 2), params.outputSize, numImages);
 
 % Backprop
-del = (sumdiff .* params.f_prime(h)) / numImages / 1000;
+del = sumdiff / numImages;
 Wgrad = del * data.imgs';
 bgrad = sum(del, 2);
 
