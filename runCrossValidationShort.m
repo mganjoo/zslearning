d0f92368ddd68567780ad613843b4a4aa49e9ff8
Milind Ganjoo <mganjoo@stanford.edu>
2012-12-17 17:34:53 -0800

    Fix bug with cell reference

--- runCrossValidationShort.m
+++ runCrossValidationShort.m
@@ -8,7 +8,7 @@ numReplicates = [15 20 25];
 for k = 1:length(wordDatasets)
     for i = 1:length(dropoutFracs)
         for j = 1:length(numReplicates)
-            trainParams.wordDataset = wordDatasets(k);
+            trainParams.wordDataset = wordDatasets{k};
             trainParams.dropoutFraction = dropoutFracs(i);
             trainParams.numReplicate = numReplicates(j);
             mapTrain;
