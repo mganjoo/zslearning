be40124591c8971892fd8fcc890e951f40bf1c06
Milind Ganjoo <mganjoo@stanford.edu>
2013-01-08 15:20:04 -0800

    Add convex hull code

--- fastCrossValidation.m
+++ fastCrossValidation.m
@@ -1,4 +1,5 @@
 trainParams.imageDataset = 'cifar10';
+trainParams.maxIter = 200;
 trainParams.dropoutFraction = 1;
 trainParams.numReplicate = 0;
 
@@ -11,7 +12,17 @@ trainParams.costFunction = @sgdOneShotCostDropout;
 train;
 
 trainParams.imageDataset = 'cifar96';
-trainParams.maxIter = 100;
+trainParams.maxIter = 400;
 trainParams.trainFunction = @trainLBFGS;
 trainParams.costFunction = @cwTrainingCost;
+train;
+
+trainParams.dropoutFraction = 1;
+trainParams.numReplicate = 0;
+trainParams.trainFunction = @trainLBFGS;
+trainParams.costFunction = @mapOneShotCostDropout;
+train;
+
+trainParams.trainFunction = @trainSGD;
+trainParams.costFunction = @sgdOneShotCostDropout;
 train;
