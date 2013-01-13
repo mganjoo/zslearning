b281f6cfbf91e52ba00d19bd6187e4689e94f64b
Milind Ganjoo <mganjoo@stanford.edu>
2013-01-13 04:08:48 -0800

    Fix access bug in train script

--- traintestGaussian.m
+++ traintestGaussian.m
@@ -7,10 +7,10 @@ addpath costFunctions/;
 numCategories = 10;
 outputPath = 'allExecs';
 
+disp('Loading data');
 load('image_data/features/cifar10/train.mat');
 load('image_data/features/cifar10/test.mat');
 load('word_data/acl/cifar10/wordTable.mat');
-load('image_data/images/meta.mat');
 
 if not(exist(outputPath, 'dir'))
     mkdir(outputPath);
@@ -19,6 +19,8 @@ end
 for z1 = 1:numCategories-1
     for z2 = z1+1:numCategories
         zeroCategories = [z1 z2];
+        disp('Zero categories:');
+        disp(zeroCategories);
         nonZeroCategories = setdiff(1:numCategories, zeroCategories);
         
         % divide into two training sets (for mapping function and threshold) 
@@ -30,25 +32,28 @@ for z1 = 1:numCategories-1
             [ ~, t ] = find(trainY == nonZeroCategories(i));
             X1(:, (i-1)*4000+1:i*4000) = trainX(:, t(1:4000));
             X2(:, (i-1)*1000+1:i*1000) = trainX(:, t(4001:end));
-            Y1((i-1)*4000+1:i*4000) = tempTrainY(t(1:4000));
-            Y2((i-1)*1000+1:i*1000) = tempTrainY(t(4001:end));
+            Y1((i-1)*4000+1:i*4000) = trainY(t(1:4000));
+            Y2((i-1)*1000+1:i*1000) = trainY(t(4001:end));
         end
         
         % permute
         order1 = randperm(32000);
         order2 = randperm(8000);
-        X1 = X1(order1);
-        X2 = X2(order2);
+        X1 = X1(:, order1);
+        X2 = X2(:, order2);
         Y1 = Y1(order1);
         Y2 = Y2(order2);
         
+        disp('Training mapping function');
         % Train mapping function
         fastTrain;
         
+        disp('Training SVM features');
         % Train SVM features
         L = 0.01;
         thetaSvm = train_svm(X1', Y1', 1/L);
         
+        disp('Training Gaussian classifier');
         % Train Gaussian classifier
         [ W, b ] = stack2param(theta, trainParams.decodeInfo);
         mapped = bsxfun(@plus, 0.5 * W{1} * X2, b{1});
