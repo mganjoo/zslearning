e8bcd70de668fb51a24f2d6fb3f0815e0889cc16
Milind Ganjoo <mganjoo@stanford.edu>
2013-01-13 12:54:05 -0800

    Modified to support CIFAR-96

--- traintestGaussian.m
+++ traintestGaussian.m
@@ -4,81 +4,94 @@ addpath toolbox/minFunc/;
 addpath toolbox/pwmetric/;
 addpath costFunctions/;
 
-numCategories = 10;
-outputPath = 'allExecs';
+dataset = 'cifar10';
+wordset = 'acl';
+trainFrac = 0.8;
+
+if strcmp(dataset, 'cifar10')
+    TOTAL_NUM_TRAIN = 50000;
+    TOTAL_NUM_PER_CATEGORY = 5000;
+    numCategories = 10;
+    % 'cat', 'truck'
+    zeroCategories = [ 4, 10 ];
+else
+    TOTAL_NUM_TRAIN = 48000;
+    TOTAL_NUM_PER_CATEGORY = 500;
+    numCategories = 96;
+    % 'boy', 'lion', 'orange', 'train', 'couch', 'house' 
+    zeroCategories = [ 12, 42, 52, 87, 26, 36 ];
+end
+outputPath = sprintf('gauss_%s_%s', dataset, wordset);
 
 disp('Loading data');
-load('image_data/features/cifar10/train.mat');
-load('image_data/features/cifar10/test.mat');
-load('word_data/acl/cifar10/wordTable.mat');
+load(['image_data/features/' dataset '/train.mat']);
+load(['image_data/features/' dataset '/test.mat']);
+load(['word_data/' wordset '/' dataset '/wordTable.mat']);
 
 if not(exist(outputPath, 'dir'))
     mkdir(outputPath);
 end
 
-% set z1 independently
-% z1 = 1;
-%for z1 = 1:numCategories-1
-    for z2 = z1+1:numCategories
-        zeroCategories = [z1 z2];
-        disp('Zero categories:');
-        disp(zeroCategories);
-        nonZeroCategories = setdiff(1:numCategories, zeroCategories);
-        
-        % divide into two training sets (for mapping function and threshold) 
-        X1 = zeros(size(trainX, 1), 32000);
-        X2 = zeros(size(trainX, 1), 8000);
-        Y1 = zeros(1, 32000);
-        Y2 = zeros(1, 8000);
-        for i = 1:length(nonZeroCategories)
-            [ ~, t ] = find(trainY == nonZeroCategories(i));
-            X1(:, (i-1)*4000+1:i*4000) = trainX(:, t(1:4000));
-            X2(:, (i-1)*1000+1:i*1000) = trainX(:, t(4001:end));
-            Y1((i-1)*4000+1:i*4000) = trainY(t(1:4000));
-            Y2((i-1)*1000+1:i*1000) = trainY(t(4001:end));
-        end
-        
-        % permute
-        order1 = randperm(32000);
-        order2 = randperm(8000);
-        X1 = X1(:, order1);
-        X2 = X2(:, order2);
-        Y1 = Y1(order1);
-        Y2 = Y2(order2);
-        
-        disp('Training mapping function');
-        % Train mapping function
-        fastTrain;
-        
-        disp('Training SVM features');
-        % Train SVM features
-        L = 0.01;
-        mappedCategories = zeroes(1, 10);
-        mappedCategories(nonZeroCategories) = 1:numCategories-2;
-        thetaSvm = train_svm(X1', mappedCategories(Y1)', 1/L)';
-        
-        disp('Training Gaussian classifier');
-        % Train Gaussian classifier
-        [ W, b ] = stack2param(theta, trainParams.decodeInfo);
-        mapped = bsxfun(@plus, 0.5 * W{1} * X2, b{1});
-        [mu, sigma, priors] = trainGaussianDiscriminant(mapped, Y2, numCategories, wordTable);
-        sortedLogprobabilities = sort(predictGaussianDiscriminant(mapped, mu, sigma, priors, zeroCategories));
+numTrain = (numCategories - length(zeroCategories)) / numCategories * TOTAL_NUM_TRAIN;
 
-        % Test
-        seenAccuracies = zeros(1, 80);
-        unseenAccuracies = zeros(1, 80);
+disp('Zero categories:');
+disp(zeroCategories);
+nonZeroCategories = setdiff(1:numCategories, zeroCategories);
 
-        for i = 1:80
-            cutoff = sortedLogprobabilities((i-1)*100+1);
+% divide into two training sets (for mapping function and threshold) 
+X1 = zeros(size(trainX, 1), trainFrac * numTrain);
+X2 = zeros(size(trainX, 1), (1 - trainFrac) * numTrain);
+Y1 = zeros(1, trainFrac * numTrain);
+Y2 = zeros(1, (1 - trainFrac) * numTrain);
+for i = 1:length(nonZeroCategories)
+    [ ~, t ] = find(trainY == nonZeroCategories(i));
+    X1(:, (i-1)*trainFrac*TOTAL_NUM_PER_CATEGORY+1:i*trainFrac*TOTAL_NUM_PER_CATEGORY) = trainX(:, t(1:trainFrac*TOTAL_NUM_PER_CATEGORY));
+    X2(:, (i-1)*(1-trainFrac)*TOTAL_NUM_PER_CATEGORY+1:i*(1-trainFrac)*TOTAL_NUM_PER_CATEGORY) = trainX(:, t((trainFrac*TOTAL_NUM_PER_CATEGORY+1:end));
+    Y1((i-1)*trainFrac*TOTAL_NUM_PER_CATEGORY+1:i*trainFrac*TOTAL_NUM_PER_CATEGORY) = trainY(t(1:trainFrac*TOTAL_NUM_PER_CATEGORY));
+    Y2((i-1)*(1-trainFrac)*TOTAL_NUM_PER_CATEGORY+1:i*(1-trainFrac)*TOTAL_NUM_PER_CATEGORY) = trainY(t(trainFrac*TOTAL_NUM_PER_CATEGORY+1:end));
+end
 
-            % Test Gaussian classifier
-            results = mapGaussianThresholdDoEvaluate( testX, testY, zeroCategories, label_names, wordTable, ...
-                theta, thetaSvm, trainParams, cutoff, mu, sigma, priors, false);
+% permute
+order1 = randperm(trainFrac * numTrain);
+order2 = randperm((1-trainFrac) * numTrain);
+X1 = X1(:, order1);
+X2 = X2(:, order2);
+Y1 = Y1(order1);
+Y2 = Y2(order2);
 
-            seenAccuracies(i) = results.seenAccuracy;
-            unseenAccuracies(i) = results.unseenAccuracy;
-            
-        end        
-        save(sprintf('%s/out_%s_%s.mat', outputPath, label_names{z1}, label_names{z2}), 'results', 'seenAccuracies', 'unseenAccuracies');
-    end
-%end
+disp('Training mapping function');
+% Train mapping function
+fastTrain;
+
+disp('Training SVM features');
+% Train SVM features
+L = 0.01;
+mappedCategories = zeroes(1, numCategories);
+mappedCategories(nonZeroCategories) = 1:numCategories-length(zeroCategories);
+thetaSvm = train_svm(X1', mappedCategories(Y1)', 1/L)';
+
+disp('Training Gaussian classifier');
+% Train Gaussian classifier
+[ W, b ] = stack2param(theta, trainParams.decodeInfo);
+mapped = bsxfun(@plus, 0.5 * W{1} * X2, b{1});
+[mu, sigma, priors] = trainGaussianDiscriminant(mapped, Y2, numCategories, wordTable);
+sortedLogprobabilities = sort(predictGaussianDiscriminant(mapped, mu, sigma, priors, zeroCategories));
+
+% Test
+seenAccuracies = zeros(1, (1-trainFrac) * numTrain / 100);
+unseenAccuracies = zeros(1, (1-trainFrac) * numTrain / 100);
+
+for i = (1-trainFrac) * numTrain / 100
+    cutoff = sortedLogprobabilities((i-1)*100+1);
+
+    % Test Gaussian classifier
+    results = mapGaussianThresholdDoEvaluate( testX, testY, zeroCategories, label_names, wordTable, ...
+        theta, thetaSvm, trainParams, cutoff, mu, sigma, priors, false);
+
+    seenAccuracies(i) = results.seenAccuracy;
+    unseenAccuracies(i) = results.unseenAccuracy;
+
+end
+zeroList = label_names(zeroCategories);
+zeroStr = [sprintf('%s_',zeroList{1:end-1}),zeroList{end}];
+save(sprintf('%s/out_%s.mat', outputPath, zeroStr), 'results', 'seenAccuracies', 'unseenAccuracies');
