0c06cac42c142012d44d9e5b10e9f3d92f068806
Richard Socher <richard@socher.org>
2013-01-20 15:40:52 -0800

    added outlier detection train and test

---
+++ outlierDetection/loOP_script.m
@@ -0,0 +1,76 @@
+%loOP_script
+
+% use this many points to define the neighborhood 
+kNN = 20;
+
+lambda = 3;
+
+% needs slmetric_pw
+addpath(genpath('D:\projects\toolbox'))
+
+
+load('ex8data1.mat');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Compute Local Outlier Probabilities  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% for data points X (trained mapped images later)
+% evaluate on Xval  (zero-shot test images later)
+
+%compute all nearest neighbors
+allDist = slmetric_pw(X',X','eucdist');
+
+%first row are just the points, then follow the nearest neighbors
+[allPointsNNDistances allPointsNN] = sort(allDist);
+
+S = allPointsNN(2:kNN+1,:);
+Sdist = allPointsNNDistances(2:kNN+1,:);
+sigma = sqrt(sum(allPointsNNDistances.^2)./kNN);
+pdist = lambda * sigma;
+% expected value: E_{s\in S(o)}[pdist(s,S(s))]
+Epdist = mean(pdist(S));
+plof = pdist./Epdist -1;
+nplof = lambda * sqrt(mean(plof.^2));
+loop = erf(plof./(nplof * sqrt(2)));
+loop(loop<0) = 0;
+
+
+% outlier threshold 
+outliers = loop>=0.5;
+%  Visualize the example dataset
+fprintf('Visualizing example dataset for outlier detection.\n\n');
+hold on
+plot(X(:, 1), X(:, 2), 'bx');
+axis([0 30 0 30]);
+
+plot(X(outliers, 1), X(outliers, 2), 'ro', 'LineWidth', 2, 'MarkerSize', 10);
+disp(['Found ' num2str(sum(outliers)) ' outliers'])
+hold off
+
+% now run for new validation/test set Xval
+% Xval = Xval(1:100,:);yval = yval(1:100);
+allDist_test = slmetric_pw(X',Xval','eucdist');
+
+% first row are just the points, then follow the nearest neighbors
+% for each test point, we look at the closest training points only
+[allPointsNNDistances_test allPointsNN_test] = sort(allDist_test);
+S_test = allPointsNN_test(2:kNN+1,:);
+Sdist_test = allPointsNNDistances_test(2:kNN+1,:);
+sigma_test = sqrt(sum(allPointsNNDistances_test.^2)./kNN);
+pdist_test = lambda * sigma_test;
+Epdist_test = mean(pdist(S_test)); 
+plof_test = pdist_test./Epdist_test -1;
+%nplof = lambda * sqrt(mean(plof_test.^2)); stays from train
+loop_test = erf(plof_test./(nplof * sqrt(2)));
+loop_test(loop_test<0) = 0;
+outliers_test = loop_test>=0.5;
+
+fprintf('Visualizing test example dataset for outlier detection.\n\n');
+hold on
+% figure;
+plot(Xval(:, 1), Xval(:, 2), 'bx');
+axis([0 30 0 30]);
+
+plot(Xval(outliers_test, 1), Xval(outliers_test, 2), 'go', 'LineWidth', 2, 'MarkerSize', 10);
+disp(['Found ' num2str(sum(outliers_test)) ' test outliers, ' num2str(sum(outliers_test==yval')) '/' num2str(length(yval)) ' correct.'])
+hold off
---
+++ visualize_script.m
@@ -0,0 +1,25 @@
+%visualize_script 
+%WARNING: Needs lots of RAM !
+
+addpath(genpath('.'))
+
+% uncomment depending on what you want to map:
+% training data (no zero shot classes) or test data (includes zero shot classes)
+%  load('mappedTrainData.mat');
+ load('mappedTestData.mat');
+
+load('word_data\acl\cifar10\wordTable.mat')
+
+numImages = size(mappedX, 2);
+t = tsne([mappedX wordTable]');
+mappedX_t = t(1:numImages, :);
+mappedWordTable_t = t(numImages+1:end, :);
+
+% load label names
+load('image_data/images/cifar10/meta.mat');
+
+% load word table
+load('word_data/acl/cifar10/wordTable.mat');
+
+% do the visualization
+visualize(mappedX_t, Y, mappedWordTable_t, label_names);
